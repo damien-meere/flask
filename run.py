a252dadd2e3a8254f5841de57e68fa22800f4d0a
Damien Meere <damien.meere@bt.com>
2019-12-15 12:04:42 +0000

    Error Fix

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def index():
     return "Hello World"
 
-id __name__ == "__main__":
+if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
