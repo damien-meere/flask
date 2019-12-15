7233fa9ac505c61318bfce88eb87ab4b92803605
Damien Meere <damien.meere@bt.com>
2019-12-15 12:48:05 +0000

    edited the code to explicitly call out the loopbck IP(127.0.0.1) and port 8080 instead of resolving from local environment

--- run.py
+++ run.py
@@ -3,11 +3,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def index():
+def hello():
     return "Hello World"
 
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
+    app.run(host="127.0.0.1",
+            port="8080",
             debug=True)
